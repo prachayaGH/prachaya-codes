255f17a7eb7a0c0be145acbd8b0001f10225aa53
unknown <Prachaya>
2025-03-16 22:13:41 +0700

    feat: add HeroSection and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import {Navbar} from './components/Navbar'
-
+import {Herosection ,Footer} from './components/Herosection'
 
 function App() {
   return (
     <>
       <section className='bg-black'>
         <Navbar />
+        <Herosection />
+        <Footer />
       </section>
     </>
   )
