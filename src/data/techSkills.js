3505b8f0ff9d34ca537b7a9c03137fee84cbd7db
unknown <Prachaya>
2025-03-19 23:05:00 +0700

    feat: add About me section

---
+++ src/data/techSkills.js
@@ -0,0 +1,41 @@
+export const techStacks = [
+    {
+        
+        id: 1,
+        image: "../public/IMG/Html_logo.png",
+        name: "HTML"
+    },
+    {
+        id: 2,
+        image: "../public/IMG/CSS_logo.png",
+        name: "CSS"
+    },
+    {
+        id: 3,
+        image: "../public/IMG/Tailwind_logo.png",
+        name: "Tailwind"
+    },
+    {
+        id: 4,
+        image: "../public/IMG/JavaScript_logo.png",
+        name: "JavaScript"
+    },
+    {
+        id: 5,
+        image: "../public/IMG/React_logo.png",
+        name: "React"
+    },
+]
+
+export const tools = [
+    {
+        id: 1,
+        image: "../public/IMG/VsCode_logo.png",
+        name: "VsCode"
+    },
+    {
+        id: 2,
+        image: "../public/IMG/Github_logo.png",
+        name: "GitHub"
+    },
+]
