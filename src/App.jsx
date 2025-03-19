846932caaad3a8bb88824b60493215cbf3b116ed
unknown <Prachaya>
2025-03-19 23:05:00 +0700

    feat: add About me section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import {Navbar} from './components/Navbar'
 import {Herosection ,Footer} from './components/Herosection'
 import {ArticleSection} from './components/ArticleSection'
+import { AboutMeSection } from './components/AboutMeSection'
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <section className='bg-black'>
         <Navbar />
         <Herosection />
+        <AboutMeSection />
         <ArticleSection />
         <Footer />
       </section>
