21a91de439463236e1afed42723c5717b750a874
Prachaya Kosolpattrapk <prachaya.ksp@gmail.com>
2025-04-22 18:09:12 +0700

    feat(server/post): add post API with validation middleware and model
    - create `middleware/validatePostBody.mjs` and `middleware/validatePostId.mjs`
    - create `model/postModel.mjs`
    - create `route/postRouter.mjs`

--- server/app.mjs
+++ server/app.mjs
@@ -1,29 +1,16 @@
 import express from "express";
 import cors from "cors";
+import postRouter from "./route/postRouter.mjs";
 
 const app = express();
 const PORT = process.env.PORT || 4001;
 
 app.use(cors());
 app.use(express.json());
+app.use("/posts", postRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});
-
-app.get("/profiles", (req, res) => {
-  try {
-    return res.status(200).json({
-      data: {
-        name: "John",
-        age: 20,
-      },
-    });
-  } catch (error) {
-    return res.status(500).json({
-      error: "Internal Server Error",
-    });
-  }
 });
 
 app.listen(PORT, () => {
