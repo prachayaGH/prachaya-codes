210ad5d3cf1f3c21b2ddf7da7949ae7de3deb4fd
Prachaya Kosolpattrapk <prachaya.ksp@gmail.com>
2025-08-20 13:26:53 +0700

    fix: update CORS origin list to include new project URL

--- server/app.mjs
+++ server/app.mjs
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 4001;
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://prachaya-codes.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://prachaya-codes.vercel.app",
+      "https://prachaya-codes-ijvipucob-prachayas-projects-273f8736.vercel.app"
+    ],
     credentials: true, // ⬅️ สำคัญมาก
   })
 );
